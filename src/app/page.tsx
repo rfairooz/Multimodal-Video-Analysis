be3f49a31368a2ac3bddcf034a1d989f7f5054f9
Raidah Fairooz <rfair008@ucr.edu>
2025-05-30 22:29:59 -0700

    set up frontend app environment

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center">
+      <p className="text-4xl text-white font-semibold">
+        Multimodal Video Project
+      </p>
+    </div>
+  );
+}
